4a2de0ecae1157b3963d7e661fd2720afbb40787
mattangoh <mattangoh@gmail.com>
2023-12-07 17:32:55 -0800

    fixed test_suite path hopefully

--- test_files/test_suite.py
+++ test_files/test_suite.py
@@ -1,7 +1,13 @@
+import os
 import unittest
 
 if __name__ == "__main__":
+    
+    #Ensuring we are located where test_suite is saved
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
+    print(f'Running tests in directory: {current_script_dir}')
+
     loader = unittest.TestLoader()
-    suite = loader.discover(start_dir='.', pattern='test_*.py')
+    suite = loader.discover(start_dir = current_script_dir, pattern='test_*.py')
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
